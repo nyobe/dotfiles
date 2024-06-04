3325d03b4ec9e0bb886d329a98e51c6bc4f6ec51
nyobe <213631+nyobe@users.noreply.github.com>
2024-06-03 19:39:03 -0700

    kakoune: basic config

--- editor.nix
+++ editor.nix
@@ -6,6 +6,36 @@
   programs.kakoune = {
     enable = true;
     defaultEditor = true;
+    config = {
+      showMatching = true;
+      wrapLines.enable = true;
+      keyMappings = [
+        {
+          mode = "normal";
+          key = "<esc>";
+          effect = ";,";
+          docstring = "clear selections";
+        }
+        {
+          mode = "user";
+          key = "<space>";
+          effect = ";,";
+          docstring = "clear selections";
+        }
+        {
+          mode = "user";
+          key = "/";
+          effect = ":comment-line<ret>";
+          docstring = "comment lines";
+        }
+        {
+          mode = "user";
+          key = "y";
+          effect = "<a-|>pbcopy<ret>";
+          docstring = "copy to clipboard";
+        }
+      ];
+    };
   };
 
   # programs.vscode.enable = true;
