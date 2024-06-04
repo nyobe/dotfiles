b8608b79a96e9e39a2c22a8d0a3f98eb1e797670
nyobe <213631+nyobe@users.noreply.github.com>
2024-06-03 19:32:23 -0700

    editor: add vscode binpath

--- editor.nix
+++ editor.nix
@@ -7,4 +7,9 @@
     enable = true;
     defaultEditor = true;
   };
+
+  # programs.vscode.enable = true;
+  home.sessionPath = [
+    "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+  ];
 }
